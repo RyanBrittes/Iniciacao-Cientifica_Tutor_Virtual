20cdbcefca83d43edfb82f926d4dab1feec95394
SAMUEL MARQUES DA COSTA <costadev19@gmail.com>
2025-11-15 09:26:12 -0400

    [FIX] -  VectorSearch object-oriented

--- App/Backend/dataBase/vectorSearch.py
+++ App/Backend/dataBase/vectorSearch.py
@@ -5,37 +5,45 @@ from App.Backend.rag.embedGenerate import EmbedGenerate
 
 load_dotenv()
 
-mongo_client = MongoClient(os.getenv("MONGO_ADDRESS"))
-db_access = mongo_client[os.getenv("MONGO_DB")]
-collection_access = db_access[os.getenv("MONGO_COLLECTION")]
-embedding = EmbedGenerate()
+class VectorSearch:
+    def __init__(self):
+        self.mongo_client = MongoClient(os.getenv("MONGO_ADDRESS"))
+        self.db_access = self.mongo_client[os.getenv("MONGO_DB")]
+        self.collection_access = self.db_access[os.getenv("MONGO_COLLECTION")]
+        self.embedding = EmbedGenerate()
 
-prompt = input('Digite a frase para busca: ')
-query = embedding.embed_query(prompt)
+    def search(self, prompt):
+        query = self.embedding.embed_query(prompt)
 
-pipeline = [
-    {
-        "$vectorSearch": {
-            "index": "vector_index",          
-            "path": "objplusembbeding",              
-            "queryVector" : query,
-            "numCandidates": 100,
-            "limit": 5
-        }
-    },
-    {
-        "$project": {
-            "_id": 0,
-            "text": 1,
-            "score": {"$meta": "vectorSearchScore"}
-        }
-    }
-]
+        pipeline = [
+            {
+                "$vectorSearch": {
+                    "index": "vector_index",
+                    "path": "objplusembbeding",
+                    "queryVector": query,
+                    "numCandidates": 100,
+                    "limit": 5
+                }
+            },
+            {
+                "$project": {
+                    "_id": 0,
+                    "text": 1,
+                    "score": {"$meta": "vectorSearchScore"}
+                }
+            }
+        ]
 
-results = list(collection_access.aggregate(pipeline))
+        results = list(self.collection_access.aggregate(pipeline))
+        return results
 
-print("\nInput →", prompt)
-print("\nResultados semelhantes:")
+if __name__ == "__main__":
+    searcher = VectorSearch()
+    prompt = input('Digite a frase para busca: ')
+    results = searcher.search(prompt)
 
-print(f"\nInput -> {prompt}")
-print(f"\nDocumentos semelhantes:\n{results}")
+    print("\nInput →", prompt)
+    print("\nResultados semelhantes:")
+
+    print(f"\nInput -> {prompt}")
+    print(f"\nDocumentos semelhantes:\n{results}")
